1ba2bb3a9fbd022f91119142526be7a7c4ac14ca
Lorenzo_Sforna <loenzosforna@gmail.com>
2026-05-26 22:27:24 +0200

    added WebView

---
+++ src/shared/SharedConstants.java
@@ -0,0 +1,14 @@
+package shared;
+
+public final class SharedConstants {
+    
+    private SharedConstants(){}
+
+    //from controller
+    public static final int FRAMES_PER_SECOND = 60;
+
+    //from model
+    public static final int MODEL_SCREEN_WIDTH = 224;
+    public static final int MODEL_SCREEN_HEIGHT = 288;
+
+}
